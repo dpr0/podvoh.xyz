b080a49d667aeec9d0698581ca9328eed5cbe6d9
dvitvitskiy <dmitriy.vitvitskiy@sbermarket.ru>
2023-01-28 17:58:27 +0300

    bump

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -5,7 +5,7 @@ Trestle.configure do |config|
   #
   # Set the page title shown in the main header within the admin.
   #
-  config.site_title = 'podvoh.xyz'
+  config.site_title = 'podvoh.krsz.ru'
 
   # Specify a custom image to be used in place of the site title for mobile and
   # expanded/desktop navigation. These images should be placed within your
